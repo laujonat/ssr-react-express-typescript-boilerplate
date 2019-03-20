cf3b3e3bae0ea5c1cb5dbf149770b310be1d41f1
Jonathan Lau <jonhlau28@gmail.com>
2019-03-20 01:05:55 -0700

    next configuration paths update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,22 @@
+/* https://github.com/zeit/next.js */ 
+const fs = require('fs')
+const { join } = require('path')
+const { promisify } = require('util')
+const copyFile = promisify(fs.copyFile)
+
 module.exports = {
-  exportPathMap: function() {
-    return {
-      '../../app/templates': { page: '/' }
+  exportPathMap: async function (
+    defaultPathMap = exportPathMap = () => {
+      return {
+        './app/templates': { page: '/' },
+      }
+    }, 
+    { dev, dir, outDir, distDir, buildId } ) {
+    if (dev) {
+      return defaultPathMap
     }
+    // This will copy robots.txt from your project root into the out directory
+    await copyFile(join(dir, 'robots.txt'), join(outDir, 'robots.txt'), join(distDir, './app/static'))
+    return defaultPathMap
   }
 }
